3cb923e3bd613e95bfe3b63cc9155ccdd369a39d
Diego Didona <didona@gsd.inesc-id.pt>
2014-04-03 12:16:06 +0100

    Tweaking KNN

--- src/main/java/diego/knn/KNNLearner_D.java
+++ src/main/java/diego/knn/KNNLearner_D.java
@@ -1,40 +1,30 @@
 package diego.knn;
 
-import eu.cloudtm.Configuration.KnnConfiguration;
+import eu.cloudtm.DataUtility.DataConverter;
 import eu.cloudtm.Knn.DataSets;
-import eu.cloudtm.Knn.Learner;
+import eu.cloudtm.autonomicManager.oracles.InputOracle;
 import eu.cloudtm.autonomicManager.oracles.Oracle;
+import eu.cloudtm.autonomicManager.oracles.OutputOracle;
+import eu.cloudtm.autonomicManager.oracles.exceptions.OracleException;
 import org.apache.log4j.Logger;
 import weka.core.DistanceFunction;
+import weka.core.Instances;
 import weka.core.neighboursearch.LinearNNSearch;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * @author Diego Didona
  * @email didona@gsd.inesc-id.pt
  * @since 03/04/14
  */
-public class KNNLearner_D extends Learner implements Oracle {
+public class KNNLearner_D extends Learner_D implements Oracle {
    static Logger logger = Logger.getLogger(KNNLearner_D.class);
    private LinearNNSearch KNN;
-   private int NumNeighbours;
-   private String ConsideredOutOracle;
+   private int numNeighbours;
+   private String targetFeature;
    private double cutoff;
-
-   private String[] getconfiguration() {
-
-      String[] param = new String[5];
-      param[0] = KnnConfiguration.getInstance().getDistanceFunction();
-      param[1] = KnnConfiguration.getInstance().getIsNormalized();
-      param[2] = KnnConfiguration.getInstance().getknearestneighbourg();
-      param[3] = KnnConfiguration.getInstance().getTarget();
-      param[4] = KnnConfiguration.getInstance().getCutoff();
-
-      return param;
-
-
-   }
 
    private String[] buildOptions(KNNConfig config) {
       return new String[]{config.getIsNormalized()};
@@ -55,8 +45,8 @@ public class KNNLearner_D extends Learner implements Oracle {
       KNNConfig config = (KNNConfig) KNNConfig.getInstance();
 
 
-      NumNeighbours = Integer.parseInt(config.getknearestneighbourg());
-      ConsideredOutOracle = config.getTarget();
+      numNeighbours = Integer.parseInt(config.getknearestneighbourg());
+      targetFeature = config.getTarget();
       cutoff = Double.parseDouble(config.getCutoff());
       KNN = new LinearNNSearch();
 
@@ -72,4 +62,52 @@ public class KNNLearner_D extends Learner implements Oracle {
    }
 
 
+   @Override
+   public OutputOracle forecast(InputOracle io) throws OracleException {
+
+      double AVGrmse = Double.MAX_VALUE;
+      Oracle best = null;
+
+      try {
+
+         Instances filtered = filterInstancesIfNeeded(m_Training);
+         KNN.setInstances(filtered);
+
+         m_TestSet = DataConverter.FromInputOracleToInstance(io);
+         logger.info("PREDICT TARGET VALUE FOR  : " + m_TestSet.toStringNoWeight());
+         System.out.println(m_TestSet);
+         Neighbourshood = KNN.kNearestNeighbours(m_TestSet, numNeighbours);//>k neighbours are returned if there are more than one neighbours at the kth boundary.
+         distances = KNN.getDistances();
+
+
+         RMSE = computeRMSE(this.targetFeature);
+
+
+         double actual;
+         for (Map.Entry<Oracle, Double[]> entry : RMSE.entrySet()) {
+
+
+            // actual=(entry.getValue()[0]*VarianceRMSE(entry.getValue())[0]+entry.getValue()[1]*VarianceRMSE(entry.getValue())[1])/2;
+            actual = (entry.getValue()[0] + entry.getValue()[1]) / 2;
+            if (actual <= AVGrmse) {
+               AVGrmse = actual;
+               best = entry.getKey();
+
+            }
+         }
+
+         logger.info("ORACLE SELECTED FOR PREDICTION : " + best.toString().split("@")[0]);
+
+         return best.forecast(io);
+
+      } catch (Exception ex) {
+         logger.error("forecast error  " + ex);
+         throw new OracleException(ex);
+      }
+   }
+
+
+   protected boolean considerInstances(int i) {
+      return distances[i] < cutoff;
+   }
 }
--- src/main/java/diego/knn/Learner_D.java
+++ src/main/java/diego/knn/Learner_D.java
@@ -1,9 +1,122 @@
 package diego.knn;
 
+import eu.cloudtm.Knn.DataSets;
+import eu.cloudtm.autonomicManager.oracles.Oracle;
+import eu.cloudtm.autonomicManager.oracles.OutputOracle;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 /**
  * @author Diego Didona
  * @email didona@gsd.inesc-id.pt
  * @since 03/04/14
  */
 public abstract class Learner_D {
+   protected Instances m_Training = null;
+   protected String m_TestSetFile = null;
+   protected Instance m_TestSet = null;
+   protected String m_TrainingFile = null;
+   protected HashMap<Oracle, Double[]> RMSE;
+   protected Instances Neighbourshood;
+   protected double[] distances;
+   protected double[][] SquaredErrors;
+
+
+   private final static Log logger = LogFactory.getLog(Learner_D.class);
+
+
+   protected Instances filterInstancesIfNeeded(Instances toFilter) {
+      //With new samples we need to do nothing
+      //In case, we can re-integrate Ennio's methods for RD and Isolation level
+      return toFilter;
+   }
+
+   /**
+    * Valid value for Parameter are: throughput abortRate responseTime everyone separated by a space
+    */
+
+   //I would not choose a learner basing on the average among the errors on different features!
+   //Every feature should have its own learner
+   protected HashMap<Oracle, Double[]> computeRMSE(String Parameter) throws Exception {
+      HashMap<Oracle, Double[]> rmse = new HashMap<Oracle, Double[]>();
+      Double[] RMSe;
+      double errorOutputRO;
+      double errorOutputWO;
+
+      Method method;
+      OutputOracle outputValidationSet;
+      OutputOracle outputOracle;
+      StringTokenizer token;
+      String outputname;
+
+      SquaredErrors = new double[2][Neighbourshood.numInstances()];
+
+      for (Map.Entry<Oracle, HashMap<Instance, OutputOracle>> entry : DataSets.predictionResults.entrySet()) {
+         double consideredInstances = 0;
+         double SEOutputRO = 0D;
+         double SEOutputWO = 0D;
+         for (int i = 0; i < Neighbourshood.numInstances(); i++) {
+
+            //Get the i-th instance from the neighborhood
+            Instance inst = DataSets.InstancesMap.get(Neighbourshood.instance(i).toStringNoWeight());
+            //Take the output from the validationSet (i.e., I guess, the REAL value)
+            outputValidationSet = DataSets.ValidationSet.get(inst);
+            //Take the --cached-- output of the Oracle
+            outputOracle = entry.getValue().get(inst);
+
+            logger.info("Instance :" + inst + "\n" + "validationOutput= " + outputValidationSet + "\n" + "Oracle Output= " + outputOracle + "\n" + "Distance= " + distances[i]);
+            token = new StringTokenizer(Parameter);
+
+            while (token.hasMoreTokens()) {
+
+               outputname = token.nextToken();
+
+               method = OutputOracle.class.getMethod(outputname, int.class);
+
+               errorOutputRO = (Double) method.invoke(outputValidationSet, 0) - (Double) method.invoke(outputOracle, 0);
+               errorOutputWO = (Double) method.invoke(outputValidationSet, 1) - (Double) method.invoke(outputOracle, 1);
+
+               if (considerInstances(i)) {//does not consider in the accuracy computation  points too distant
+                  consideredInstances++;
+                  SEOutputRO = SEOutputRO + Math.pow(errorOutputRO, 2);
+                  logger.info("error on " + outputname + "RO prediction for " + entry.getKey().toString().split("@")[0] + " = " + errorOutputRO);
+
+
+                  SEOutputWO = SEOutputWO + Math.pow(errorOutputWO, 2);
+                  logger.info("error on " + outputname + "WO prediction for " + entry.getKey().toString().split("@")[0] + " = " + errorOutputWO);
+
+
+                  SquaredErrors[0][i] = SEOutputRO;
+                  SquaredErrors[1][i] = SEOutputWO;
+
+
+               } else {
+                  logger.info("error on " + outputname + "WO prediction for " + entry.getKey().toString().split("@")[0] + " = " + errorOutputWO);
+                  logger.info("error on " + outputname + "RO prediction for " + entry.getKey().toString().split("@")[0] + " = " + errorOutputRO);
+                  logger.info("point not considered during accuracy computation due to the high AVG contribution (" + distances[i] + "> 1* ");
+               }
+
+            }
+
+         }
+         RMSe = new Double[2];
+         RMSe[0] = Math.sqrt(SEOutputRO) / consideredInstances;
+         RMSe[1] = Math.sqrt(SEOutputWO) / consideredInstances;
+         logger.info("RESULT considering " + Parameter + " of  " + entry.getKey().toString().split("@")[0] + ":" + "RMSERO =" + RMSe[0] + "  RMSEWO =" + RMSe[1]);
+         rmse.put(entry.getKey(), RMSe);
+      }
+      return rmse;
+   }
+
+
+   protected abstract boolean considerInstances(int i);
+
+
 }
